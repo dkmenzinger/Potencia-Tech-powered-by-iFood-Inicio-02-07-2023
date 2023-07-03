476ef3906cb246f938544e1e716f81cebda83861
dkmenzinger <rogeriodasilvamenzinger@gmail.com>
2023-07-02 22:03:43 -0400

    Create variaveis_constantes.py

---
+++ variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Guilherme"
+idade = 28
+
+nome = "Giovana"
+
+print(nome, idade)
+
+limite_saque_diario = 1000 # O padrão de nomes deve ser snake case.
+                           # Escolher nomes sugestivos.
+
+STATES = ["SP", "RJ", "SC", "RS"] # Nome de constantes todo em maiúsculo.
+print(STATES)

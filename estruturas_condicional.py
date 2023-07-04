540cd6cbceb664d0ff00d49010aa205fa740acfe
dkmenzinger <rogeriodasilvamenzinger@gmail.com>
2023-07-04 08:55:45 -0400

    Estruturas Condicionais e de Repetição em Python

---
+++ estruturas_condicional.py
@@ -0,0 +1,26 @@
+MAIOR_IDADE = 18
+IDADE_ESPECIAL = 16
+
+idade = int(input("informe sua idade:"))
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH.")
+
+if idade < MAIOR_IDADE:
+    print("Ainda não pode tirar a CNH.")
+
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH.")
+else:
+    print("Ainda não pode tirar a CNH.")
+
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH.")
+elif idade == IDADE_ESPECIAL:
+    print("Pode fazer aulas teóricas, mas não pode fazer aulas práticas.")
+else:
+    print("Ainda não pode tirar a CNH.")
+
+

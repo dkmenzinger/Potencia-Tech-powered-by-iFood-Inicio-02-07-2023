8b3ba8a5c9d499f01c2d5163ee6a2a9218221c2e
dkmenzinger <rogeriodasilvamenzinger@gmail.com>
2023-07-10 11:25:36 -0400

    Create listas_1.py

---
+++ estrutura_de_dados/Listas/listas_1.py
@@ -0,0 +1,19 @@
+#  Listas:
+#  Em Python podem armazenar de maneira sequencial qualquer tipo de objeto.
+#  Podemos criar listas utilizando o construtor list,
+#  a função range ou colocando valores separados por vírgula dentro de conlchetes.
+#  Listas são objetos mutáveis, portando podemos alterar seus valores após a criação.
+
+frutas = ["laranja", "maca", "uva", "pera"]
+print(frutas)
+
+frutas = []
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari","F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)

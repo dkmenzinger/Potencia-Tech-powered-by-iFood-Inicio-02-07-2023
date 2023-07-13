28a0e354ef6f28fa26aa5e5d8852734f4db95dc3
dkmenzinger <rogeriodasilvamenzinger@gmail.com>
2023-07-13 15:03:30 -0400

    estrutura_de_dados

---
+++ estrutura_de_dados/Tuplas/tuplas.py
@@ -0,0 +1,15 @@
+# Tuplas são estruturas de dados muito parecidas com as litas,a
+# principal diferença é que tuplas são imutáveis equanto listas
+# são mutáveis.
+
+frutas = ("laranja", "pera", "uva",)
+print(frutas[-1])
+print(frutas[-3])
+print(frutas)
+
+letras = tuple("Python")
+
+numeros = tuple([1, 2, 3, 4])
+
+pais = ("Brasil",)
+

405f054bb8a4e8a0846fb99f9c28053cd7034842
dkmenzinger <rogeriodasilvamenzinger@gmail.com>
2023-07-15 08:03:48 -0400

    estrutura_de_dados

---
+++ estrutura_de_dados/Conjuntos/set.py
@@ -0,0 +1,14 @@
+print(set([1, 2, 3, 1, 3, 4])) # {1, 2, 3, 4}
+
+letras = set("abacaxi") # {'b', 'i', 'x', 'a', 'c'}
+print(letras)
+
+carros = set(("palio", "gol", "celta", "palio")) # {'celta', 'gol', 'palio'}
+print(carros)
+
+numeros = {1, 2, 3, 2}
+print(numeros)
+
+numeros = list(numeros)
+
+print(numeros[0])
--- estrutura_de_dados/Tuplas/tuplas.py
+++ estrutura_de_dados/Tuplas/tuplas.py
@@ -13,3 +13,4 @@ numeros = tuple([1, 2, 3, 4])
 
 pais = ("Brasil",)
 
+
